d884afa50e5991a34be0e32b2cd7dd704a2423b7
cuong <ntc.uit@gmail.com>
2026-06-01 21:30:03 +0700

    build(ui): add navigation-compose, hilt-navigation, lifecycle-compose and material icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,12 +49,17 @@ android {
 dependencies {
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.activity.compose)
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
+    implementation(libs.compose.material.icons)
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation.compose)
     debugImplementation(libs.compose.ui.tooling)
 
     implementation(libs.hilt.android)
