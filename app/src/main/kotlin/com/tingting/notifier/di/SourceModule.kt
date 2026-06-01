927fe343aba15d18418dc141bbe74f60256a29e7
cuong <ntc.uit@gmail.com>
2026-06-01 20:20:54 +0700

    feat(di): provide parser, processor, dedupe gate, IO dispatcher

---
+++ app/src/main/kotlin/com/tingting/notifier/di/SourceModule.kt
@@ -0,0 +1,40 @@
+package com.tingting.notifier.di
+
+import com.tingting.notifier.parser.TransactionParser
+import com.tingting.notifier.source.notification.DedupeGate
+import com.tingting.notifier.source.notification.NotificationProcessor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+/** Qualifier for the IO dispatcher used by detection background work. */
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SourceModule {
+
+    @Provides
+    @Singleton
+    fun provideTransactionParser(): TransactionParser = TransactionParser()
+
+    @Provides
+    @Singleton
+    fun provideNotificationProcessor(parser: TransactionParser): NotificationProcessor =
+        NotificationProcessor(parser)
+
+    @Provides
+    @Singleton
+    fun provideDedupeGate(): DedupeGate = DedupeGate()
+
+    @Provides
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
